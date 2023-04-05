4ac44d1af0d045e420407123b5c7cfcae2bb9957
Sbrussee <Siemenbrussee@hotmail.com>
2023-04-05 11:13:56 +0200

    23

--- GVAE.py
+++ GVAE.py
@@ -1255,8 +1255,8 @@ for epoch in range(1, args.epochs+1):
         batch.expr.fill_(0.0)
         assert batch.expr.sum() < 0.1
     else:
-        batch.expr[cells].index_fill_(0, torch.tensor(cells), 0.0)
-        assert batch.expr[cells].sum() < 0.1
+        batch.expr.index_fill_(0, torch.tensor(cells), 0.0)
+        assert batch.expr[cells, :].sum() < 0.1
     for cell in cells:
         loss = train_model(model, batch, pyg_graph.expr[cell], cell, pyg_graph.weight)
         total_loss_over_cells += loss
@@ -1278,8 +1278,8 @@ for epoch in range(1, args.epochs+1):
         val_batch.expr.fill_(0)
         assert val_batch.expr.sum() < 0.1
     else:
-        val_batch.expr[cells].index_fill_(0, torch.tensor(cells), 0.0)
-        assert val_batch.expr[val_cells].sum() < 0.1
+        val_batch.expr.index_fill_(0, torch.tensor(val_cells), 0.0)
+        assert val_batch.expr[vall_cells, :].sum() < 0.1
     for cell in cells:
         val_loss, x_hat = validate(model, val_batch, pyg_graph.expr[cell], cell, pyg_graph.weight)
         total_r2 += r2_score(pyg_graph.expr[cell].cpu(), x_hat.cpu())
