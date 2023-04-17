e85268ee3a4268537f6b01b7bf11dca3b34ea808
Sbrussee <Siemenbrussee@hotmail.com>
2023-04-17 12:32:15 +0200

    yes

--- GVAE.py
+++ GVAE.py
@@ -706,7 +706,7 @@ def plot_latent(model, pyg_graph, anndata, cell_types, device, name, number_of_c
 
     #Plot per cell type:
     for celltype in cell_types:
-        idx_to_plot = anndata[anndata.obs[celltype_key] == celltype].obs.index.to_list()
+        idx_to_plot = anndata[anndata.obs[celltype_key] == celltype].obs.index
 
         tsne = manifold.TSNE(n_components=2)
         tsne_z =tsne.fit_transform(z[idx_to_plot,:])
