f2f35fd7dcf0f4885b9a87057bcf80962d149b94
Sbrussee <Siemenbrussee@hotmail.com>
2023-04-18 09:47:38 +0200

    bug

--- GVAE.py
+++ GVAE.py
@@ -709,7 +709,7 @@ def plot_latent(model, pyg_graph, anndata, cell_types, device, name, number_of_c
 
     #Plot per cell type:
     for celltype in cell_types:
-        idx_to_plot = [x for x in anndata[anndata.obs[celltype_key] == celltype].obs.index.get_indexer(anndata.obs_names) if x not -1]
+        idx_to_plot = [x for x in anndata[anndata.obs[celltype_key] == celltype].obs.index.get_indexer(anndata.obs_names) if x != -1]
         print(idx_to_plot)
 
         celltype = celltype.replace('/', '_')
