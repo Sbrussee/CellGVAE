0f355153ddf2c2a571054c2dfdf013b230899266
Sbrussee <Siemenbrussee@hotmail.com>
2023-04-18 11:33:58 +0200

    yes

--- GVAE.py
+++ GVAE.py
@@ -710,7 +710,7 @@ def plot_latent(model, pyg_graph, anndata, cell_types, device, name, number_of_c
     #Plot per cell type:
     for celltype in cell_types:
         idx_to_plot = anndata[anndata.obs[celltype_key] == celltype].obs_names
-        print([idx_to_plot)
+        print(idx_to_plot)
 
 
         celltype = celltype.replace('/', '_')
