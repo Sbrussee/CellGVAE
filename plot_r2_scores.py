90d39d8db3ab421c9f21fa62eb1c52ee870ac445
Sbrussee <Siemenbrussee@hotmail.com>
2023-04-24 11:11:21 +0200

    yes

--- plot_r2_scores.py
+++ plot_r2_scores.py
@@ -4,4 +4,4 @@ dataset_name = 'seqfish'
 for exp in ['exp2', 'exp3', 'exp4', 'exp5', 'exp6']:
     with open(exp+".pkl", 'rb') as f:
         r2_dict = pickle.load(f)
-    plot_r2_scores(r2_dict, 'core_model', dataset_name)
+    plot_r2_scores(r2_dict, 'core_model', dataset_name+exp)
