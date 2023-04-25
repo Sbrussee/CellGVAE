5c47acaed2ceccfd6d258565f20333c2fcf209c4
Sbrussee <Siemenbrussee@hotmail.com>
2023-04-25 11:32:02 +0200

    yes

--- plot_r2_scores.py
+++ plot_r2_scores.py
@@ -1,7 +1,7 @@
 import pickle
 from GVAE import plot_r2_scores
 dataset_name = 'seqfish'
-for exp in ['exp2', 'exp3', 'exp4', 'exp5', 'exp6']:
+for exp in ['exp2', 'exp3', 'r2_neighbors', 'r2_prediction_mode', 'r2_thresholds']:
     try:
         with open(exp+".pkl", 'rb') as f:
             r2_dict = pickle.load(f)
