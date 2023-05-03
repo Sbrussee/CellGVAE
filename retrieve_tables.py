2d61b96f4f6905701ff3cdcd6fa5b7577c1def21
Sbrussee <Siemenbrussee@hotmail.com>
2023-05-03 11:06:03 +0200

    yes

--- retrieve_tables.py
+++ retrieve_tables.py
@@ -9,9 +9,8 @@ for file in files:
     print(f"FILE: {file}")
     df = pd.read_pickle(file)
     if df != None:
-        mask = df['pvalues'] < 0.05
         selected = df['pvalues'][(df['pvalues'] < 0.01).any(axis=1)]
-        selected['count'] =  selected.lt(0.01).sum(axis=1)
+        selected['count'] =  selected.lt(0.001).sum(axis=1)
         sorted = selected.sort_values(by='count', ascending=False)
         print(sorted[:10])
         sq.pl.ligrec(df, pvalue_threshold=0.001, remove_empty_interactions=True,
