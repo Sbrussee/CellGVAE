94d1c8b366d39e2b1daee8da67c74018f5845592
Sbrussee <Siemenbrussee@hotmail.com>
2023-05-03 10:04:34 +0200

    yes

--- retrieve_tables.py
+++ retrieve_tables.py
@@ -1,6 +1,7 @@
- import glob
- import pandas as pd
+import glob
+import pandas as pd
 
+#Read all ligrec pickle fles
 files = glob.glob('*ligrec*.pkl')
 for file in files:
     df = pd.read_pickle(file)
