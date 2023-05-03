bcd5df4cd3720beedf2eb99174c0e9b94669c74f
Sbrussee <Siemenbrussee@hotmail.com>
2023-05-03 11:09:29 +0200

    yes

--- retrieve_tables.py
+++ retrieve_tables.py
@@ -9,6 +9,7 @@ for file in files:
     print(f"FILE: {file}")
     df = pd.read_pickle(file)
     if df != None:
+        file = file.rstrip(".pkl")
         selected = df['pvalues'][(df['pvalues'] < 0.01).any(axis=1)]
         selected['count'] =  selected.lt(0.001).sum(axis=1)
         sorted = selected.sort_values(by='count', ascending=False)
@@ -16,3 +17,4 @@ for file in files:
         sq.pl.ligrec(df, pvalue_threshold=0.001, remove_empty_interactions=True,
                      remove_nonsig_interactions=True, alpha=0.0001, means_range=(0.3, np.inf),
                       save=f"{file}.png")
+        sorted[:10].to_csv(f"{file}.csv")
