f0805a3218f045670f6f7e72fe7af5023429e4e9
Sbrussee <Siemenbrussee@hotmail.com>
2023-05-08 11:49:14 +0200

    yes

--- run_on_merfish.py
+++ run_on_merfish.py
@@ -132,7 +132,7 @@ apply_tsne(dataset.X.toarray(), f"tSNE of {name} data", f"tsne_{name}", dataset,
 apply_umap(dataset.X.toarray(), f"UMAP of {name} data", f"umap_{name}", dataset, celltype_key)
 variance_decomposition(dataset.X.toarray(), celltype_key, name)
 
-sc.pl.spatial(dataset, use_raw=False, spot_size=0.1, title=f'Spatial celltype distribution',
+sc.pl.spatial(dataset, use_raw=False, spot_size=0.01, title=f'Spatial celltype distribution',
           save=f"spatial_scatter_{name}.png", color=celltype_key, show=False)
 plt.close()
 #Train the model on all data
