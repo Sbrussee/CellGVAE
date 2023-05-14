5de1e2cd77246a5cc29e247709f3a29c6850a9b4
Sbrussee <Siemenbrussee@hotmail.com>
2023-05-14 14:29:55 +0200

    yes

--- GVAE.py
+++ GVAE.py
@@ -2397,7 +2397,7 @@ if __name__ == '__main__':
     if args.weight:
         G, isolates = convert_to_graph(dataset.obsp['spatial_distances'], dataset['X'], dataset.obs[celltype_key], name+'_train', args=args)
     else:
-        G, isolates = convert_to_graph(dataset.obsp['spatial_connectivities'], dataset['X'],, dataset.obs[celltype_key], name+'_train', args=args)
+        G, isolates = convert_to_graph(dataset.obsp['spatial_connectivities'], dataset['X'], dataset.obs[celltype_key], name+'_train', args=args)
     G = nx.convert_node_labels_to_integers(G)
 
     pyg_graph = pyg.utils.from_networkx(G)
