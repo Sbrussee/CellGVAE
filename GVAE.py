782951bac167e234338af0539d8a0cbfd84b172d
Sbrussee <Siemenbrussee@hotmail.com>
2023-05-15 12:28:26 +0200

    yes

--- GVAE.py
+++ GVAE.py
@@ -2059,7 +2059,9 @@ def train(model, pyg_graph, optimizer_list, train_i, val_i, k, args, discriminat
             #Add MSE for Inner Product Decoder
             ipd = InnerProductDecoder().to(device)
             A_hat = ipd.forward_all(torch.from_numpy(get_latent_space_vectors(model, pyg_graph.to(device), dataset, device, args=args)).to(device))
+            print(A_hat)
             A = torch.from_numpy(to_scipy_sparse_matrix(pyg_graph.edge_index).toarray())
+            print(A)
             total_loss_over_cells += np.mean((A - A_hat.cpu()) ** 2)
         batch = batch.cpu()
         cells_seen += len(cells)
