f9d5cbb5b25881d51f5b449ab1e6cdd39b2a051c
Sbrussee <Siemenbrussee@hotmail.com>
2023-05-23 11:51:37 +0200

    yes

--- plot_lr_interactions.py
+++ plot_lr_interactions.py
@@ -7,6 +7,7 @@ pattern = 'lr_*exp6*.csv'
 files = glob.glob(pattern)
 print(files)
 for file in files:
+  print(file)
   df = pd.read_csv(file)
   print(df)
   df['Count'] = (df < 0.001).sum(axis=1)
