78cd2fb48403d0e2282e88f44c34fc41be216b7f
Sbrussee <Siemenbrussee@hotmail.com>
2023-05-23 10:45:07 +0200

    fixed name

--- plot_lr_interactions.py
+++ plot_lr_interactions.py
@@ -4,11 +4,12 @@ import pandas
 import numpy
 import glob
 
-pattern = '/lr*.pkl'
+pattern = '/ligrec*.pkl'
 files = glob.glob(pattern)
 
 for file in files:
   interactions = pd.read_pickle(file)
+  name = file
   sq.pl.ligrec(interactions, pvalue_threshold=0.001, remove_empty_interactions=True,
                remove_nonsig_interactions=True, alpha=0.0001, means_range=(0.3, np.inf),
                 save=f"lr_true_{name}.png")
