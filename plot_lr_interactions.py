27015b12c8f978e48121f9afeafbc9a6fca2df9d
Sbrussee <Siemenbrussee@hotmail.com>
2023-05-23 10:42:57 +0200

    added LR interaction plotting function

---
+++ plot_lr_interactions.py
@@ -0,0 +1,14 @@
+import squidpy as sq
+import scanpy as sc
+import pandas
+import numpy
+import glob
+
+pattern = '/lr*.pkl'
+files = glob.glob(pattern)
+
+for file in files:
+  interactions = pd.read_pickle(file)
+  sq.pl.ligrec(interactions, pvalue_threshold=0.001, remove_empty_interactions=True,
+               remove_nonsig_interactions=True, alpha=0.0001, means_range=(0.3, np.inf),
+                save=f"lr_true_{name}.png")
