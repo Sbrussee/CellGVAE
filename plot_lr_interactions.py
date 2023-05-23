4955a4f547b71b8ee15cd081f6ee46156ee25ecc
Sbrussee <Siemenbrussee@hotmail.com>
2023-05-23 11:38:06 +0200

    yes

--- plot_lr_interactions.py
+++ plot_lr_interactions.py
@@ -10,8 +10,8 @@ files = glob.glob(os.getcwd()+pattern)
 
 for file in files:
   interactions = pd.read_pickle(file)
-  print(interactions)
+  print()
   name = file
-  sq.pl.ligrec(interactions, pvalue_threshold=0.01, #remove_empty_interactions=True,
-               remove_nonsig_interactions=True, alpha=0.0001, means_range=(0.3, np.inf),
+  sq.pl.ligrec(interactions, pvalue_threshold=0.01, remove_empty_interactions=True,
+               remove_nonsig_interactions=True, alpha=0.001, means_range=(0.3, np.inf),
                 save=f"lr_{name}.png")
