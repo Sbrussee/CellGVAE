3a0fbfa77b8120468b5cab0ab3b218a239fd0894
Sbrussee <Siemenbrussee@hotmail.com>
2023-05-23 11:45:11 +0200

    ye

--- plot_lr_interactions.py
+++ plot_lr_interactions.py
@@ -1,13 +1,11 @@
-import squidpy as sq
-import scanpy as sc
 import pandas as pd
 import numpy as np
 import glob
 import os
 
-pattern = '/lr*.csv'
+pattern = '/lr_*.csv'
 files = glob.glob(os.getcwd()+pattern)
-
+print(files)
 for file in files:
   df = pd.read_csv(pattern)
   df['Count'] = (df < 0.001).sum(axis=1)
